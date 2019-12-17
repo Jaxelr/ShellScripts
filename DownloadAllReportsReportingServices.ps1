dcd9c74d6a420f7ddb24dbb2616b462fe1dda8cd
jaxelr <jaxelr@gmail.com>
2019-12-17 11:41:24 -0400

    Added shell script to download all reports from a report server

---
+++ DownloadAllReportsReportingServices.ps1
@@ -0,0 +1,11 @@
+﻿#To install Reporting Tools.
+#Invoke-Expression (Invoke-WebRequest https://aka.ms/rstools)
+
+#Declare SSRS URI
+$sourceRsUri = 'http://localhost/ReportServer/ReportService2010.asmx?wsdl'
+
+#Declare Proxy so we dont need to connect with every command
+$proxy = New-RsWebServiceProxy -ReportServerUri $sourceRsUri
+
+#Output ALL Catalog items to file system
+Out-RsFolderContent -Proxy $proxy -RsFolder / -Destination 'C:\temp' -Recurse
