7fda9a0ff7a003efa21d476d30a5012bea73997c
jaxelr <jaxelr@gmail.com>
2020-01-02 12:13:25 -0400

    refactor: parameterized the destination folder of the download script

--- DownloadAllReportsReportingServices.ps1
+++ DownloadAllReportsReportingServices.ps1
@@ -1,11 +1,15 @@
 ﻿#To install Reporting Tools.
 #Invoke-Expression (Invoke-WebRequest https://aka.ms/rstools)
+#This is also on github (https://github.com/Microsoft/ReportingServicesTools)
 
 #Declare SSRS URI
-$sourceRsUri = 'http://localhost/ReportServer/ReportService2010.asmx?wsdl'
+$sourceRsUri = 'http://PSM-SRV-12/ReportServer/ReportService2010.asmx?wsdl'
+
+#Select Your Destination Path
+$destination = 'C:\temp'
 
 #Declare Proxy so we dont need to connect with every command
 $proxy = New-RsWebServiceProxy -ReportServerUri $sourceRsUri
 
 #Output ALL Catalog items to file system
-Out-RsFolderContent -Proxy $proxy -RsFolder / -Destination 'C:\temp' -Recurse
+Out-RsFolderContent -Proxy $proxy -RsFolder / -Destination $destination -Recurse
