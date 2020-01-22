0a8b2fff909647d5536291ac89c9e01facb40c19
jaxelr <jaxelr@gmail.com>
2020-01-22 15:45:45 -0400

    Added GetCliaFromNpi to obtain the Clias from the Npi

---
+++ GetCliaFromNpi.ps1
@@ -0,0 +1,25 @@
+[CmdletBinding()]
+Param(
+    # Input Path for the Processing Logic
+    [Parameter(Mandatory=$True,Position=1)]
+    [string]$InputFile,
+
+    # Input Path for the Processing Logic
+    [Parameter(Mandatory=$True,Position=2)]
+    [string]$OutputFile
+)
+
+# This is incredibly client specific
+
+$lines = Get-Content $InputFile;
+$url = "https://npiprofile.com/npi/";
+
+Set-Content -Path $OutputFile -Value "Npi|Clia";
+
+foreach($line in $lines.Split('\n'))
+{
+    $response = Invoke-WebRequest $url$line;
+    $InnerValue = $response.AllElements | Where-Object {$_.TagName -eq "TD" -and  $_.innerHtml -match "^40D*" } | Select-Object -ExpandProperty InnerHTML;
+    $result = $line + "|" + $InnerValue;
+    Add-Content -Path $OutputFile -Value $result;
+}
