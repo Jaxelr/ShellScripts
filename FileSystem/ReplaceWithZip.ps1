24de9213fb89d4e8009d1d85718c82ea6f84b1aa
Jaxel Rojas <jaxelr@gmail.com>
2021-11-09 09:01:15 -0400

    Added ReplaceWithZip script

---
+++ FileSystem/ReplaceWithZip.ps1
@@ -0,0 +1,20 @@
+[CmdletBinding()]
+Param(
+    [Parameter(Mandatory=$True, Position=1)]
+    [string]$Source
+)
+
+## Sample usage of the shell
+## cd to this folder...
+## .\ReplaceWithZip.ps1 -Source "C:\temp\*.csv"
+
+if (!(Test-Path $Source)) {
+    Write-Host -Message "No files found on wildcard $Source, Check path. Could be a permission issue?!"
+    Exit
+}
+
+foreach ($file in Get-ChildItem -Path $Source -Recurse) {
+    $Zip = Join-Path -Path (Split-Path $file) -ChildPath (-Join ((Split-Path $file -Leaf), '.zip'))
+    Compress-Archive -Path $file -DestinationPath $Zip  -Force
+    Remove-Item -Path $file
+}
