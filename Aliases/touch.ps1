7a586006a3c486f367bb4c4bccc316cee7df0671
Jaxel Rojas <jaxelr@gmail.com>
2022-03-22 12:13:03 -0400

    feat: added alias of touch to create empty files quickly

---
+++ Aliases/touch.ps1
@@ -0,0 +1,20 @@
+function Touch-File() {
+    $fileName = $args[0]
+    # Check of the file exists
+    if (-not(Test-Path $fileName)) {
+        # It does not exist. Create it
+        New-Item -ItemType File -Name $fileName
+    }
+    else {
+        #It exists. Update the timestamp
+        (Get-ChildItem $fileName).LastWriteTime = Get-Date
+    }
+}
+
+### Create an alias for touch
+
+# Check if the alias exists
+if (-not(Test-Path -Path Alias:touch)) {
+    New-Alias -Name touch Touch-File -Force
+    Write-Host "Alias Touch created."
+}
