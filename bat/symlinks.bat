ff3cd224242637f784363202eca6574b493444a3
Jaxel Rojas <jrojaslopez@microsoft.com>
2022-04-27 22:04:53 -0400

    fix: symlink path

--- bat/symlinks.bat
+++ bat/symlinks.bat
@@ -1,6 +1,6 @@
 REM These are symlinks from my work environment
 mklink /J "C:\Users\jrojaslopez\src" ".\OneDrive - Microsoft\src"
 mklink /J "C:\Users\jrojaslopez\Documents" ".\OneDrive - Microsoft\Documents"
-mklink /J "C:\Users\jrojaslopez\" ".\OneDrive - Microsoft\bin"
+mklink /J "C:\Users\jrojaslopez\bin" ".\OneDrive - Microsoft\bin"
 mklink /J "C:\Users\jrojaslopez\Downloads" ".\OneDrive - Microsoft\Downloads"
 
