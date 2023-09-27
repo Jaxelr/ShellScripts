06f20588c6679be3126dddcc8c8e1ada1de5cbe2
Jaxel Rojas <jaxelr@gmail.com>
2023-09-27 09:26:13 -0400

    fix: winget dependencies for work environment

--- PkgManagers/Winget/work.ps1
+++ PkgManagers/Winget/work.ps1
@@ -1,4 +1,4 @@
-winget install -e --id Docker.DockerDesktop
+winget install -e --id RedHat.Podman-Desktop
 winget install -e --id Microsoft.VisualStudioCode
 winget install -e --id mcmilk.7zip-zstd
 winget install -e --id Fork.Fork
@@ -11,6 +11,7 @@ winget install -e --id VideoLAN.VLC
 winget install -e --id GoLang.Go
 winget install -e --id SublimeHQ.SublimeText.4
 winget install -e --id Insomnia.Insomnia
+winget install -e --id icsharpcode.ILSpy
 winget install -e --id Mozilla.Firefox
 winget install -e --id Python.Python.3
 winget install -e --id Microsoft.PowerToys
@@ -23,6 +24,7 @@ winget install -e --id Microsoft.AzureDataStudio
 winget install -e --id Canonical.Ubuntu
 winget install -e --id Microsoft.AzureCosmosEmulator
 winget install -e --id Microsoft.VisualStudio.2022.Enterprise
+winget install -e --id Microsoft.SQLServerManagementStudio
 Install-Module posh-git -Scope CurrentUser
 Install-Module Terminal-Icons -Scope CurrentUser
 Install-Module PsReadline -Scope CurrentUser
