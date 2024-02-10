f8e2bda2436008c6c77fc8e45c9dbae255b43a02
Jaxel Rojas <jaxelr@gmail.com>
2024-02-10 08:06:03 -0400

    chore: update work list of sw to install

--- PkgManagers/Winget/work.ps1
+++ PkgManagers/Winget/work.ps1
@@ -3,11 +3,12 @@ winget install -e --id Microsoft.VisualStudioCode
 winget install -e --id mcmilk.7zip-zstd
 winget install -e --id Fork.Fork
 winget install -e --id Rustlang.Rust.GNU
-winget install -e --id LINQPad.LINQPad.7
+winget install -e --id LINQPad.LINQPad.8
 winget install -e --id JanDeDobbeleer.OhMyPosh
+winget install -e --id dotPDNLLC.paintdotnet
 winget install -e --id Git.Git
+winget install -e --id GitHub.cli
 winget install -e --id Spotify.Spotify
-winget install -e --id VideoLAN.VLC
 winget install -e --id GoLang.Go
 winget install -e --id SublimeHQ.SublimeText.4
 winget install -e --id Insomnia.Insomnia
@@ -20,11 +21,13 @@ winget install -e --id CrystalRich.LockHunter
 winget install -e --id Microsoft.WindowsTerminal
 winget install -e --id Microsoft.PowerShell
 winget install -e --id OpenJS.NodeJS.LTS
+winget install -e --id DenoLand.Deno
 winget install -e --id Microsoft.AzureDataStudio
-winget install -e --id Canonical.Ubuntu
 winget install -e --id Microsoft.AzureCosmosEmulator
+winget install -e --id Microsoft.Azure.StorageExplorer
 winget install -e --id Microsoft.VisualStudio.2022.Enterprise
 winget install -e --id Microsoft.SQLServerManagementStudio
+winget install -e --id Microsoft.AzureCLI
 Install-Module posh-git -Scope CurrentUser
 Install-Module Terminal-Icons -Scope CurrentUser
 Install-Module PsReadline -Scope CurrentUser
